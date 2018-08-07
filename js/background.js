820d6c31254ee75bf8e2d47a0166dd9c4fdc3dff
gwuah <griffith@dolphtech.com>
2018-08-07 14:39:57 +0000

    removed unnecessary code and add new functionality

--- js/background.js
+++ js/background.js
@@ -13,41 +13,36 @@ const MyStore = function (){
   }
 };
 
-var RuleMatcher = function(rules){
-  var lastRequestId;
+// chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+//   chrome.tabs.sendMessage(tabs[0].id, {content: JSON.stringify(details)}, function(response) {
+//     console.log(response)
+//   });  
+// });
 
-  this.rules = rules;
-
-  this.redirectOnMatch = function(request){
-    var rule = _.find(rules, function(rule){ 
-      return request.url.indexOf(rule.from) > -1 
-      && request.requestId !== lastRequestId; 
-    });
-
-    if(rule){
-      lastRequestId = request.requestId;
-      return {
-          redirectUrl : request.url.replace(rule.from, rule.to)
-      };
-    }
-  };
-};
-
-const getNewStore = function() {
-  return new MyStore();
-}
-
-
-const matcher = getNewStore();
-const rules = matcher.getMaps();
-const ruleMatcher = new RuleMatcher(rules);
-
-
+let lastReqId = null;
 
 chrome.webRequest.onBeforeRequest.addListener(
   function(details) {
-    console.log('requesting', details)
-    return ruleMatcher.redirectOnMatch(details)
+    const urlObject = new URL(details.url);
+    // get maps from store
+    const store = new MyStore();
+    const maps = store.getMaps();
+    
+
+    // find particlar mapping in our store
+    const mapping = maps.find(mapping => urlObject.host == mapping.from)
+
+    if ((mapping) && (details.requestId !== lastReqId)) {
+      // set the request it to the latest one
+      lastReqId = details.requestId;
+      console.log(urlObject.host == mapping.from)
+      console.log(urlObject.host, mapping.from, mapping.to)
+      console.log('redirect_URL', `${mapping.to}/${urlObject.search}`);
+
+      return {
+        redirectUrl: `${mapping.to}/${urlObject.search}`
+      }
+    }
   }, {
     urls : ["<all_urls>"]
   }, ["blocking"]
